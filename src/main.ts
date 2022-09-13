3b05666a02d67cbd87567538dfe12d647224ace8
Muaaz Khan <muaaz@onpar.in>
2022-09-14 00:22:18 +0530

    Changes Updates

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.enableCors();
   // console.log(app);
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs', app, document);
 
   const server = await app.listen(process.env.PORT || 3000);
   server.keepAliveTimeout = 65000;
--- src/utils/utils.controller.ts
+++ src/utils/utils.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import { AwsFileUploadDto } from './dto/utils.dto';
 import { UtilsService } from './utils.service';
+import { diskStorage } from 'multer';
+import { Express } from 'express';
+
 @ApiTags('Utils')
 @Controller('utils')
 export class UtilsController {
   constructor(private readonly utilsservice: UtilsService) {}
 
-  @Post()
+  @Post('/aws/file-upload')
   uploadImages(@Body() data: AwsFileUploadDto) {
     return this.utilsservice.uploadfiles(data.fileName, data.fileExtention);
   }
