d609d49fd439dde7d7b2dd14fa85bd9360159cd4
Muaaz Khan <muaaz@onpar.in>
2022-09-30 13:33:50 +0530

    Update utils

--- src/utils/utils.controller.ts
+++ src/utils/utils.controller.ts
@@ -6,17 +6,24 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiExcludeController,
+  ApiExcludeEndpoint,
+  ApiHideProperty,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AwsFileUploadDto } from './dto/utils.dto';
 import { UtilsService } from './utils.service';
 import { diskStorage } from 'multer';
 import { Express } from 'express';
 
-@ApiTags('Utils')
 @Controller('utils')
+@ApiExcludeController()
 export class UtilsController {
   constructor(private readonly utilsservice: UtilsService) {}
 
+  @ApiHideProperty()
+  @ApiExcludeEndpoint()
   @Post('/aws/file-upload')
   uploadImages(@Body() data: AwsFileUploadDto) {
     return this.utilsservice.uploadfiles(data.fileName, data.fileExtention);
