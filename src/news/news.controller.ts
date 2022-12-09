79b7ca2aff8e48153d92a117267827f08c0c66c4
KhannDev <muaaz@onpar.in>
2022-12-09 17:33:21 +0530

    update

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -11,7 +11,7 @@ export class NewsController {
   constructor(private readonly newsservice: NewsService) {}
 
   /**
-   * Create News
+   * Creates News
    */
 
   @ApiOperation({ summary: 'Create News' })
